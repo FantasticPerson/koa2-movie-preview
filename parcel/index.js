2f0eeedd0ad2fde3e9383f8e92703e7ffe7bb135
wdd119 <1240172019@qq.com>
2018-10-26 00:03:51 +0800

    feat: parcel 打包

---
+++ parcel/index.js
@@ -0,0 +1,8 @@
+function changeTitle(){
+    window.$('#app').html('Parcel 大宝宝')
+}
+
+
+setTimeout(function(){
+    changeTitle()
+},2000)
---
+++ parcel/server.js
@@ -0,0 +1,9 @@
+const koa = require('koa')
+const {resolve} = require('path')
+const serve = require('koa-static')
+
+const app = new koa()
+
+app.use(serve(resolve(__dirname,'./')))
+
+app.listen(4466)
