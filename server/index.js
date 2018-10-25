5a60ca34cdb39c00b9ba3bac231b75eb766f5986
wdd119 <1240172019@qq.com>
2018-10-25 22:26:38 +0800

    feat：继承模板引擎中间件

--- server/index.js
+++ server/index.js
@@ -1,17 +1,33 @@
+// const koa = require('koa')
+// const app = new koa()
+// const pug = require('pug')
+// const {htmlTpl,ejsTpl,pugTpl} = require('./tpl')
+// const ejs = require('ejs')
+// app.use(async (ctx,next)=>{
+//     // ctx.body = ejs.render(ejsTpl,{
+//     //     you:'Luke',
+//     //     me:'Scott'
+//     // })
+//     ctx.body=pug.render(pugTpl,{
+//         you:'Luke',
+//         me:'Scott'
+//     })
+//     ctx.type = 'text/html;charset=utf-8'
+// })
+// app.listen(4455)
+
 const koa = require('koa')
 const app = new koa()
-const pug = require('pug')
-const {htmlTpl,ejsTpl,pugTpl} = require('./tpl/index')
-const ejs = require('ejs')
+const views = require('koa-views')
+const {resolve} = require('path')
+
+app.use(views(resolve(__dirname,'./views'),{
+    extension:'pug'
+}))
 app.use(async (ctx,next)=>{
-    // ctx.body = ejs.render(ejsTpl,{
-    //     you:'Luke',
-    //     me:'Scott'
-    // })
-    ctx.body=pug.render(pugTpl,{
+    await ctx.render('index',{
         you:'Luke',
         me:'Scott'
     })
-    ctx.type = 'text/html;charset=utf-8'
 })
 app.listen(4455)
--- server/tpl/pug.js
+++ server/tpl/pug.js
@@ -7,7 +7,7 @@ html
   title Koa server pug
   link(href="https://cdn.bootcss.com/twitter-bootstrap/4.1.3/css/bootstrap.min.css",rel="stylesheet")
   script(src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js")
-  script src("https://cdn.bootcss.com/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js")
+  script (src="https://cdn.bootcss.com/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js")
 
  body
   .container
